e72a38f34fb2084aeee57456b463b6227b84c7a7
ivanfg9 <56409211+ivanfg9@users.noreply.github.com>
2021-03-01 17:14:17 +0100

    Add files via upload

---
+++ PhysicsSimulator/src/simulator/model/MovingTowardsFixedPoint.java
@@ -0,0 +1,17 @@
+package simulator.model;
+
+import java.util.List;
+
+import simulator.misc.Vector2D;
+
+public class MovingTowardsFixedPoint implements ForceLaws {
+
+	private static final double g = 9.81;
+	private static final Vector2D d = new Vector2D();
+	
+	@Override
+	public void apply(List<Body> bs) {
+		
+	}
+
+}
