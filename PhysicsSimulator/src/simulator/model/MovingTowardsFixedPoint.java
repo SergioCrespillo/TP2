10faa6e6dacb8f6651e31ccd988116f8ab8e07c8
ivanfg9 <56409211+ivanfg9@users.noreply.github.com>
2021-03-01 18:33:38 +0100

    Add files via upload

--- PhysicsSimulator/src/simulator/model/MovingTowardsFixedPoint.java
+++ PhysicsSimulator/src/simulator/model/MovingTowardsFixedPoint.java
@@ -6,12 +6,27 @@ import simulator.misc.Vector2D;
 
 public class MovingTowardsFixedPoint implements ForceLaws {
 
-	private static final double g = 9.81;
-	private static final Vector2D d = new Vector2D();
+	protected double g;
+	protected Vector2D c;
+	
+	public MovingTowardsFixedPoint(double gravedad, Vector2D direccion) {
+		g = gravedad;
+		c = direccion;
+	}
 	
 	@Override
 	public void apply(List<Body> bs) {
-		
+		for(Body bi:bs) {
+			Vector2D f;
+			
+			if(c.getX() == 0 && c.getY() == 0) {
+				f = bi.getPosition().direction().scale((-g)*bi.getMass());
+			}
+			else {
+				f = c.minus(bi.getPosition().direction()).scale(bi.getMass()*(-g)); 
+			}
+			
+			bi.addForce(f);
+		}
 	}
-
 }
--- PhysicsSimulator/src/simulator/model/NewtonUniversalGravitation.java
+++ PhysicsSimulator/src/simulator/model/NewtonUniversalGravitation.java
@@ -2,10 +2,38 @@ package simulator.model;
 
 import java.util.List;
 
+import simulator.misc.Vector2D;
+
 public class NewtonUniversalGravitation implements ForceLaws {
 
+	private double G;
+	
+	public NewtonUniversalGravitation(double g) {
+		G = g;
+	}
+	
 	@Override
 	public void apply(List<Body> bs) {
+		for(Body bi:bs) {
+			for(Body bj: bs) {
+				if(!(bi.equals(bj))){
+					double fij = forceIJ(bi,bj);
+					Vector2D d = bj.getPosition().minus(bi.getPosition());
+					Vector2D Fij = d.direction().scale(fij);
+					
+					bi.addForce(Fij);
+				}
+			}
+		}
+	}
 	
+	private double forceIJ(Body bi, Body bj) {
+		double fij;
+		Vector2D pos;
+		
+		pos = bj.getPosition().minus(bi.getPosition());
+		fij = G * (bi.getMass()*bj.getMass())/Math.pow(pos.magnitude(), 2);
+		
+		return fij;
 	}
 }
